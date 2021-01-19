c736fb27812e4ab25c32efaacb1d94e663b6c450
MURA199918 <muralisaranr18@gmail.com>
2021-01-19 15:37:10 +0530

    [Murali]Refactored to create addressbook spring project to cater REST Request from addressbook UI

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook Application");
 		SpringApplication.run(AddressBookAppApplication.class, args);
 	}
 
