2f5cf7862c142cf7cc13d779c663a6ee863d5a62
MURA199918 <muralisaranr18@gmail.com>
2021-01-20 12:35:36 +0530

    [Murali]Refactored to use lombak library for logging using annotation @Slf4j at class level and logging levels, files and patterns are handled in application.properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -3,12 +3,15 @@ package com.bridgelabz.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to AddressBook Application");
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("AddressBook Application Started");
 	}
 
 }
