ff5a64c5e97fd9ea3c9d93e1ae14470364f2be7a
MURA199918 <muralisaranr18@gmail.com>
2021-01-20 13:02:38 +0530

    [Murali]Refactored to provide user friendly error response in case validation fails by creating custom exception handlers class and use @ControllerAdvice annotation so that spring framework can call this class during REST API requests

--- src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionalHandler.java
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionalHandler.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.bridgelabz.addressbookapp.dto.ResponseDTO;
 
 
+@ControllerAdvice
 public class AddressBookExceptionalHandler {
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
