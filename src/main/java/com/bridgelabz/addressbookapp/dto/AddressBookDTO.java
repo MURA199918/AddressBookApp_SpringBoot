965b42ae7987a9d166966ccd56db5df7c298d984
MURA199918 <muralisaranr18@gmail.com>
2021-01-21 15:55:46 +0530

    [Murali]Refactored to add remaining properties to payroll DTO and model and validate fields

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,32 +1,34 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-public class AddressBookDTO {
+import lombok.ToString;
+
+public @ToString class AddressBookDTO {
 	
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Contact name Invalid")
 	public String firstName;
+	
+	@NotBlank(message = "LastName cannot be Empty")
 	public String lastName;
+	
+	@NotBlank(message = "Address cannot be Empty")
 	public String address;
+	
+	@NotBlank(message = "City cannot be Empty")
 	public String city;
+	
+	@NotBlank(message = "State cannot be Empty")
 	public String state;
+	
+	@NotBlank(message = "Zip Code cannot be Empty")
 	public long zip;
+	
+	@Pattern(regexp = "((^\\\\+?)(([0-9]{2,3})(\\\\s))?)' + '[0-9]{10}$", message = "Phone Number Invalid")
 	public long phone;
+	
+	@NotBlank(message = "Email cannot be Empty")
 	public String email;
-	
-	public AddressBookDTO(String firstName, String lastName, String address, String city, String state, long zip, long phone, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phone = phone;
-		this.email = email;
-	}
-	
-	public String toString() {
-		return "firstName=" + firstName + ":lastName=" + lastName + ":Address=" + address + ":City=" + city + ":State=" + state + ":Zip=" + zip + ":Phone=" + phone + ":Email=" + email;
-	}
 	
 }
--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -2,7 +2,9 @@ package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 
-public class AddressBookData {
+import lombok.Data;
+
+public @Data class AddressBookData {
 	
 	private int contactId;
 	private String firstName;
@@ -16,6 +18,10 @@ public class AddressBookData {
 	
 	public AddressBookData(int contactId, AddressBookDTO addBookDTO) {
 		this.contactId = contactId;
+	    this.updateAddressBookData(addBookDTO);
+	}
+	
+	public void updateAddressBookData(AddressBookDTO addBookDTO) {
 		this.firstName = addBookDTO.firstName;
 		this.lastName = addBookDTO.lastName;
 		this.address = addBookDTO.address;
@@ -24,77 +30,5 @@ public class AddressBookData {
 		this.zip = addBookDTO.zip;
 		this.phone = addBookDTO.phone;
 		this.email = addBookDTO.email;
-	}
-	
-	public int getContactId() {
-		return contactId;
-	}
-	
-	public void setContactId(int contactId) {
-		this.contactId = contactId;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-	
-	public void setCity(String city) {
-		this.city = city;
-	}
-	
-	public String getState() {
-		return state;
-	}
-	
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	public long getZip() {
-		return zip;
-	}
-	
-	public void setZip(long zip) {
-		this.zip = zip;
-	}
-	
-	public long getPhone() {
-		return phone;
-	}
-	
-	public void setPhone(long phone) {
-		this.phone = phone;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
 	}
 }
